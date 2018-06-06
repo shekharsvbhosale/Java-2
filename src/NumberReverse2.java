d6af58553d07cba81e568210b2fe7c87e37d51c9
Chilja Speransky <magiclanzelot@gmx.net>
2018-06-06 11:41:33 +0200

    src folder added

--- src/NumberReverse2.java
+++ src/NumberReverse2.java
@@ -1,3 +1,4 @@
+package src;
 
 class NumberReverse2 {
 	public static void main (String args[]) {
--- src/RNATranscription.java
+++ src/RNATranscription.java
@@ -1,3 +1,4 @@
+package src;
 
 class RNATranscription {
 	public static void main(String args[]) {
