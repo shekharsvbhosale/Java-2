9ef927b6da0b6b09319371d21371bc0eec9b9d39
Chilja Speransky <magiclanzelot@gmx.net>
2018-06-06 11:06:48 +0200

    initialize repo with first 3 exercises

---
+++ ArmstrongNumber.java
@@ -0,0 +1,26 @@
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		int a = 1533;
+		int length = Integer.toString(a).length();
+		int result = 0;
+		int c = 0;
+		int digits[] = new int[length];
+		
+		//put all digits in an array as chars
+		for(int i = 0; i < digits.length; i++) {
+			//convert the extracted char literal to an int for storing in the array
+			digits[i] = Integer.parseInt(Integer.toString(a).substring(i, i+1));
+			result = result + (int) Math.pow((double) digits[i], (double) length - c);
+		}
+		
+		System.out.println("The number has " + length + " digits\n");
+		if(result == a) {
+			System.out.println("The number " + a + " is an armstrong number!");
+		} else {
+			System.out.println("The number " + a + " is not armstrong number, the result is: " + result + ".");
+		}
+			
+	}
+
+}
