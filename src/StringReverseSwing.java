2e22086a77c4ef56b85c05ded5dfb0a82cffdaa4
Chilja Speransky <magiclanzelot@gmx.net>
2018-06-08 14:19:42 +0200

    Reverse a String with StringBuilder reverse() and Swing input and output

--- src/StringReverseSwing.java
+++ src/StringReverseSwing.java
@@ -1,23 +1,22 @@
 package src;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JTextField;
-import java.awt.GridLayout;
-import javax.swing.SpringLayout;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.border.EmptyBorder;
+
+/* Reverse a String with StringBuilder method reverse()
+ * input via Swing
+ * output via Swing
+ */
 
 public class StringReverseSwing extends JFrame {
 
